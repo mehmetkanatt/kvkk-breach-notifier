b9f9a816d3bb082d1ba3edc9870113d0ed402946
Mehmet Kanat <mehmet.kanat@protonmail.com>
2026-01-16 22:08:36 +0300

    Fix link formatting in notification message

--- kvkk-breach-notifier.php
+++ kvkk-breach-notifier.php
@@ -20,7 +20,7 @@ foreach ($leaks as $leak) {
     }
 
     $title = urlencode($leak['title']);
-    $message .= "• <https://www.kvkk.gov.tr{$leak['link']}|$title>\n";
+    $message .= "• <{$leak['link']}|$title>\n";
 }
 chatPostMessage($channel, $message, $botToken);
 
